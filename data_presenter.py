cc836e27944fb7406e959ab3dcab66b2eebab32e
Devin Fischer <devinfischer32@gmail.com>
2021-08-09 22:40:43 -0600

    finished

--- data_presenter.py
+++ data_presenter.py
@@ -20,15 +20,16 @@ for line in open_file:
     line = line.split(',')
     if line[2] =="Chocolate":
       chocolate += int(line[3]) * float(line[4])
-    elif line[2] =="Strawberry":
-      strawberry += int(line[3]) * float(line[4])
     elif line[2] =="Vanilla":
       vanilla += int(line[3]) * float(line[4])
+    elif line[2] =="Strawberry":
+      strawberry += int(line[3]) * float(line[4])
+    
 
-total_invoices = strawberry+vanilla+chocolate
+total_invoices = chocolate + vanilla + strawberry
 print(round(total_invoices,2))
-print(strawberry,chocolate,vanilla)
+print(chocolate,vanilla,strawberry)
 
 fig = go.Figure(data=go.Bar(y=[chocolate, vanilla, strawberry],x=["Chocolate", "Vanilla", "Strawberry"]))
-
 fig.show()
+open_file.close()
